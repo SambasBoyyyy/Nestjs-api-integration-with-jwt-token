500bf340b601e922c0336c771e8ea245c20a630e
Samiul Basir <samiulbasirbhuiyan1234@gmail.com>
2023-07-01 16:37:41 +0600

    signin token recevies

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,13 +5,15 @@ import { AuthController } from "./auth.controller";
 import { AuthDto } from "./dto";
 import * as argon from 'argon2'
 import { PrismaClientKnownRequestError, PrismaClientUnknownRequestError, PrismaClientValidationError } from "@prisma/client/runtime";
-import { async } from "rxjs";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+
 
 @Injectable()
 
 export class AuthService{
 
-    constructor(private prisma:PrismaService){}
+    constructor(private prisma:PrismaService, private jwt:JwtService, private config:ConfigService){}
 
 async signup(dto: AuthDto){
 
@@ -47,6 +49,8 @@ catch (error) {
 
 }    
 
+
+
 async signin(dto: AuthDto){
 
 const user = await this.prisma.user.findUnique({
@@ -68,12 +72,32 @@ if(!pwMatches){
 }
 
 
-delete user.hash;
+// delete user.hash;
 
 
-    return user;
+    return this.signToken(user.id,user.email);
 }
 
+
+ async signToken( userId:number,  email:string):Promise<{access_token: string}>{
+
+    const payload = {
+        sub:userId,
+        email,
+    };
+
+    const token = await this.jwt.signAsync(payload,{
+        expiresIn: '15m',
+        secret: this.config.get('JWT_SECRET')
+        
+    })
+
+    return {
+        access_token : token,
+    }
+
+
+}
 
 
 
